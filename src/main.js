8e35085e5f292045e470c2165fe0509f597da974
Omega Paulo <paulodafiotiomega@gmail.com>
2021-12-01 16:02:35 +0200

    Done with the side nav bar on the mobile version

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+const menu = document.querySelector('.ham .ham__box--inner');
+menu.addEventListener('click', () => {
+  document.querySelector('.ham .ham__box--inner').classList.toggle('active');
+  document.querySelector('nav').classList.toggle('toggle-entrance');
+});
+
 let scroll = new SmoothScroll('a[href*="#"]', {
   speed: 700,
-  offset: 100,
+  offset: 200,
 });
