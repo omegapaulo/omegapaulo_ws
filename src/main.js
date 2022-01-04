f6e7bcfe7a336e087391e3d22fceed2161f71238
Omega Paulo <paulodafiotiomega@outlook.com>
2022-01-04 09:40:49 +0200

    Almost done with the mobile version

--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ let scroll = new SmoothScroll('a[href*="#"]', {
   speed: 700,
   offset: 100,
 });
+
+document.querySelector('.ham__box--inner').addEventListener('click', () => {
+  document.querySelector('.ham__box--inner').classList.toggle('active');
+  document.querySelector('.nav__list--mobile').classList.toggle('insideout');
+  console.log('click');
+});
